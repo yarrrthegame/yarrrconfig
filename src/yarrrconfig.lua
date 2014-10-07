1e8a76a5de3576ff6669e13edf6ed458583e8f22
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-10-07 12:34:19 +0200

    implement ship creator
     * it creates objects with physical behavior
     * it has inventory
     * it has a collider
     * it has damagecauser
     * it has loot dropper
     * it is deleted when destroyed
     * it has shape behavior
     * it has shape graphics
     * it adds listed extra behaviors
     * it adds listed tiles to the shape

--- src/yarrrconfig.lua
+++ src/yarrrconfig.lua
@@ -1,7 +1,35 @@
 local yarrrconfig = {}
 
-function yarrrconfig.create_ship()
-  return Object.new()
+function create_shape_from( tiles )
+  local local_shape = Shape.new()
+  for i, tile  in ipairs( tiles ) do
+    local_shape:add_tile( tile )
+  end
+
+  return local_shape
+end
+
+function add_behaviors_to( ship, behaviors )
+  for i, behavior  in ipairs( behaviors ) do
+    ship:add_behavior( behavior )
+  end
+end
+
+function yarrrconfig.create_ship( tiles, additional_behaviors )
+  ship = Object.new()
+  add_behaviors_to( ship, {
+      PhysicalBehavior.new(),
+      Inventory.new(),
+      Collider.new( ship_layer ),
+      DamageCauser.new( 100 ),
+      LootDropper.new(),
+      DeleteWhenDestroyed.new(),
+      ShapeBehavior.new( create_shape_from( tiles ) ),
+      ShapeGraphics.new()
+    } )
+
+  add_behaviors_to( ship, additional_behaviors )
+  return ship
 end
 
 return yarrrconfig
--- test/shipcreator_spec.lua
+++ test/shipcreator_spec.lua
@@ -1,15 +1,107 @@
 local yarrrconfig = require "yarrrconfig"
 
 local test_object = {}
-_G.Object = { new = function() return test_object end }
+function test_object:add_behavior( behavior ) end
+
+_G.Object = {
+  new = function() return test_object end }
+
+_G.PhysicalBehavior = {
+  new = function() return "physical_behavior" end }
+
+_G.Inventory = {
+  new = function() return "inventory" end }
+
+_G.Collider = {
+  new = function() return "collider" end }
+
+_G.DamageCauser = {
+  new = function() return "damagecauser" end }
+
+_G.LootDropper = {
+  new = function() return "lootdropper" end }
+
+_G.DeleteWhenDestroyed = {
+  new = function() return "deletewhendestroyed" end }
+
+local shape = {
+  add_tile = function() end }
+
+_G.Shape = {
+  new = function() return shape end }
+
+_G.ShapeBehavior = {
+  new = function() return "shapebehavior" end }
+
+_G.ShapeGraphics = {
+  new = function() return "shapegraphics" end }
+
+_G.ship_layer = "ship layer"
 
 describe( "ship creator", function()
 
-  it( "creates objects", function()
-    local s = spy.on( Object, "new" )
-    local an_object = yarrrconfig.create_ship()
-    assert.spy( s ).was.called( 1 )
-    assert.are.equal( test_object, an_object )
+  describe( "the created object", function()
+    local object_new = spy.on( Object, "new" )
+    local object_add_behavior = spy.on( test_object, "add_behavior" )
+    local collider_new = spy.on( Collider, "new" )
+    local extra_behaviors = { "first extra behavior", "second extra behavior" }
+    local tiles = { "first tile", "second tile" }
+    local shape_add_tile = spy.on( shape, "add_tile" )
+    local shape_behavior_new = spy.on( ShapeBehavior, "new" )
+
+    local an_object = yarrrconfig.create_ship( tiles, extra_behaviors )
+
+    it( "is an object", function()
+      assert.spy( object_new ).was.called( 1 )
+      assert.are.equal( test_object, an_object )
+    end)
+
+    it( "adds listed extra behaviors to the object", function()
+      for i, behavior in ipairs( extra_behaviors ) do
+        assert.spy( object_add_behavior ).was_called_with( test_object, behavior )
+      end
+    end)
+
+    it( "adds listed tiles to the shape", function()
+      for i, tile in ipairs( tiles ) do
+        assert.spy( shape_add_tile ).was_called_with( shape, tile )
+      end
+      assert.spy( shape_behavior_new ).was_called_with( shape )
+    end)
+
+    it( "has physical behavior", function()
+      assert.spy( object_add_behavior ).was_called_with( test_object, "physical_behavior" )
+    end)
+
+    it( "has inventory", function()
+      assert.spy( object_add_behavior ).was_called_with( test_object, "inventory" )
+    end)
+
+    it( "has collider", function()
+      assert.spy( collider_new ).was_called_with( ship_layer )
+      assert.spy( object_add_behavior ).was_called_with( test_object, "collider" )
+    end)
+
+    it( "has damage causer", function()
+      assert.spy( object_add_behavior ).was_called_with( test_object, "damagecauser" )
+    end)
+
+    it( "has loot dropper", function()
+      assert.spy( object_add_behavior ).was_called_with( test_object, "lootdropper" )
+    end)
+
+    it( "is deleted when destroyed", function()
+      assert.spy( object_add_behavior ).was_called_with( test_object, "deletewhendestroyed" )
+    end)
+
+    it( "has shape behavior", function()
+      assert.spy( object_add_behavior ).was_called_with( test_object, "shapebehavior" )
+    end)
+
+    it( "has shape shapegraphics", function()
+      assert.spy( object_add_behavior ).was_called_with( test_object, "shapegraphics" )
+    end)
+
   end)
 
 end)
